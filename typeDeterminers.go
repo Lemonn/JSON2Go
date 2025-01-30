7bd4c661b701749a89352078de2eeef7320abea1
Philipp Lehmann <philipp.lehmann@medionmail.com>
2025-01-30 14:24:19 +0100

    Add UUIDTypeChecker

--- typeDeterminers.go
+++ typeDeterminers.go
@@ -2,6 +2,7 @@ package JSON2Go
 
 import (
 	"github.com/araddon/dateparse"
+	"github.com/google/uuid"
 	"go/ast"
 )
 
@@ -83,6 +84,70 @@ func (t *TimeTypeChecker) GenerateToTypeFunction(functionScaffold *ast.FuncDecl)
 					},
 					&ast.Ident{
 						Name: "nil",
+					},
+				},
+			},
+		},
+	}
+	return functionScaffold
+}
+
+type UUIDTypeChecker struct{}
+
+func (u *UUIDTypeChecker) GetType() string {
+	return "uuid.UUID"
+}
+
+func (u *UUIDTypeChecker) CouldTypeBeApplied(seenValues []string) bool {
+	var err error
+	for _, value := range seenValues {
+		_, err = uuid.Parse(value)
+		if err != nil {
+			return false
+		}
+	}
+	return true
+}
+
+func (u *UUIDTypeChecker) GenerateFromTypeFunction(functionScaffold *ast.FuncDecl) *ast.FuncDecl {
+	functionScaffold.Body = &ast.BlockStmt{
+		List: []ast.Stmt{
+			&ast.ReturnStmt{
+				Results: []ast.Expr{
+					&ast.CallExpr{
+						Fun: &ast.SelectorExpr{
+							X: &ast.Ident{
+								Name: "uuid",
+							},
+							Sel: &ast.Ident{
+								Name: "Parse",
+							},
+						},
+						Args: []ast.Expr{
+							&ast.Ident{
+								Name: "baseValue",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return functionScaffold
+}
+
+func (u *UUIDTypeChecker) GenerateToTypeFunction(functionScaffold *ast.FuncDecl) *ast.FuncDecl {
+	functionScaffold.Body = &ast.BlockStmt{
+		List: []ast.Stmt{
+			&ast.ExprStmt{
+				X: &ast.CallExpr{
+					Fun: &ast.SelectorExpr{
+						X: &ast.Ident{
+							Name: "baseValue",
+						},
+						Sel: &ast.Ident{
+							Name: "String",
+						},
 					},
 				},
 			},
