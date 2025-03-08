3183953e43e35e4c4be80ffae72ff50e94a5a495
Philipp Lehmann <philipp.lehmann@medionmail.com>
2025-03-08 16:10:15 +0100

    backup

--- pkg/fieldData/file.go
+++ pkg/fieldData/file.go
@@ -8,45 +8,57 @@ import (
 	"strings"
 )
 
-type FilePath string
+type Path string
 
-func (f FilePath) IsFile() (bool, error) {
-	if len(f) == 0 {
-		return false, errors.New("file path is empty")
+func (p Path) PrependElement(element string) Path {
+	return Path(element + "." + string(p))
+}
+
+func (p Path) AppendElement(element string) Path {
+	return Path(string(p) + "." + element)
+}
+
+func (p Path) Prepend(path Path) Path {
+	return path + "." + p
+}
+
+func (p Path) Append(path Path) Path {
+	return p + "." + path
+}
+
+func (p Path) IsRootPath() bool {
+	if len(strings.Split(string(p), ".")) == 1 {
+		return true
 	}
-	if f[0] != '/' {
-		return true, nil
+	return false
+}
+
+func (p Path) GetParentPath() Path {
+	pathElements := strings.Split(string(p), ".")
+	if len(pathElements) > 1 {
+		return Path(strings.Join(pathElements[:len(pathElements)-1], "."))
 	} else {
-		return false, nil
+		return p
 	}
 }
 
-func (f FilePath) AddInFront(filePath FilePath) (FilePath, error) {
-	isFile, err := f.IsFile()
-	if err != nil {
-		return "", err
-	} else if isFile {
-		return "", errors.New("file path is a file, not a path")
-	}
-	return FilePath(strings.ReplaceAll(string(filePath+f), "//", "")), nil
+func (p Path) GetParentFieldName() string {
+	pathElements := strings.Split(string(p), ".")
+	return pathElements[len(pathElements)-2]
 }
 
-func (f FilePath) AddInBack(filePath FilePath) (FilePath, error) {
-	isFile, err := filePath.IsFile()
-	if err != nil {
-		return "", err
-	} else if isFile {
-		return "", errors.New("file path is a file, not a path")
-	}
-	return FilePath(strings.ReplaceAll(string(filePath+f), "//", "")), nil
+func (p Path) GetFieldName() string {
+	pathElements := strings.Split(string(p), ".")
+	return pathElements[len(pathElements)-1]
 }
 
 type File struct {
 	File           *ast.File         `json:"file,omitempty"`
 	FSet           *token.FileSet    `json:"fSet,omitempty"`
-	Imports        []string          `json:"extraImports,omitempty"`
-	FileEnding     *string           `json:"fileType,omitempty"`
+	Imports        Imports           `json:"extraImports,omitempty"`
+	FileEnding     string            `json:"fileType,omitempty"`
 	ModFileContent []*ModFileContent `json:"modFileContent,omitempty"`
+	Name           string
 }
 
 func GetEmptyGoFile() *File {
@@ -54,20 +66,20 @@ func GetEmptyGoFile() *File {
 	return &File{
 		File:           f,
 		FSet:           fs,
-		Imports:        nil,
-		FileEnding:     utils.StringToPointer("go"),
-		ModFileContent: nil,
+		Imports:        Imports{},
+		FileEnding:     "go",
+		ModFileContent: []*ModFileContent{},
 	}
 }
 
-func GetGoFile(decls []ast.Decl, imports []string, mfc []*ModFileContent) *File {
+func GetGoFile(decls []ast.Decl, imports Imports, mfc []*ModFileContent) *File {
 	f, fs := utils.GetEmptyAstFile("placeholder")
 	f.Decls = decls
 	return &File{
 		File:           f,
 		FSet:           fs,
 		Imports:        imports,
-		FileEnding:     utils.StringToPointer("go"),
+		FileEnding:     "go",
 		ModFileContent: mfc,
 	}
 }
@@ -103,21 +115,21 @@ type ModFileContent struct {
 
 type Imports []*Import
 
-func (i Imports) AdjustPathsIfNeeded(path string) {
+func (i Imports) AdjustPathsIfNeeded(path Path) {
 	for _, im := range i {
 		im.AdjustPathIfNeeded(path)
 	}
 }
 
 type Import struct {
-	Path            string  `json:"name"`
+	Path            Path    `json:"name"`
 	Alias           *string `json:"alias"`
 	NeedsAdjustment bool    `json:"needsAdjustment"`
 }
 
-func (i *Import) AdjustPathIfNeeded(path string) {
+func (i *Import) AdjustPathIfNeeded(path Path) {
 	if !i.NeedsAdjustment {
 		return
 	}
-	i.Path = path + i.Path
+	i.Path.Prepend(path)
 }
