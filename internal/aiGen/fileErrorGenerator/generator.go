1ea64e358e16f2d547e6082f24e6133d07282bdc
Philipp Lehmann <philipp.lehmann@medionmail.com>
2025-03-19 16:35:33 +0100

    backup

--- internal/aiGen/fileErrorGenerator/generator.go
+++ internal/aiGen/fileErrorGenerator/generator.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"go/ast"
+	"github.com/dave/dst"
+	"github.com/dave/dst/decorator"
 	"go/parser"
-	"go/printer"
 	"go/token"
 	"io"
 	"io/ioutil"
@@ -50,19 +50,17 @@ func collectTypesAndMethods(files []string) (map[string]*TypeInfo, error) {
 		if err != nil {
 			return nil, err
 		}
-		parsedFile, err := parser.ParseFile(fset, file, src, parser.AllErrors)
+		parsedFile, err := decorator.ParseFile(fset, file, src, parser.AllErrors)
 		if err != nil {
 			return nil, err
 		}
 
-		// Get package name from file's directory
 		dir := filepath.Dir(file)
 		packageName := filepath.Base(dir)
 
-		// Traverse the AST to collect types and their methods
-		ast.Inspect(parsedFile, func(n ast.Node) bool {
+		dst.Inspect(parsedFile, func(n dst.Node) bool {
 			switch x := n.(type) {
-			case *ast.TypeSpec:
+			case *dst.TypeSpec:
 				typeName := x.Name.Name
 				key := fmt.Sprintf("%s.%s", packageName, typeName)
 				if _, exists := typeInfos[key]; !exists {
@@ -72,36 +70,33 @@ func collectTypesAndMethods(files []string) (map[string]*TypeInfo, error) {
 						Methods:     make(map[string]*MethodInfo),
 					}
 				}
-			case *ast.FuncDecl:
+			case *dst.FuncDecl:
 				if x.Recv != nil && len(x.Recv.List) > 0 {
-					// Get receiver type
 					var typeName string
 					switch recvType := x.Recv.List[0].Type.(type) {
-					case *ast.StarExpr:
-						// Pointer receiver
-						if ident, ok := recvType.X.(*ast.Ident); ok {
+					case *dst.StarExpr:
+						if ident, ok := recvType.X.(*dst.Ident); ok {
 							typeName = ident.Name
 						}
-					case *ast.Ident:
-						// Value receiver
+					case *dst.Ident:
 						typeName = recvType.Name
 					}
+
 					methodName := x.Name.Name
 
-					// Count number of parameters and results
 					numParams := 0
 					if x.Type.Params != nil {
 						for _, param := range x.Type.Params.List {
 							numParams += len(param.Names)
 						}
 					}
+
 					numResults := 0
 					if x.Type.Results != nil {
 						for _, result := range x.Type.Results.List {
 							if result.Names != nil {
 								numResults += len(result.Names)
 							} else {
-								// Anonymous result (no name)
 								numResults++
 							}
 						}
@@ -159,79 +154,129 @@ func generateUnmarshalJSON(fileErrorTypes []*TypeInfo, receiverType string) (str
 		"ReceiverType":   receiverType,
 		"FileErrorTypes": fileErrorTypes,
 	}
-
 	t, err := template.New("unmarshalJSON").Parse(tmpl)
 	if err != nil {
 		return "", err
 	}
-
 	var buf bytes.Buffer
 	err = t.Execute(&buf, data)
 	if err != nil {
 		return "", err
 	}
-
 	return buf.String(), nil
 }
 
-func insertOrReplaceMethod(file *ast.File, receiverType, methodName, generatedCode string) error {
+/*
+func insertOrReplaceMethod(file *dst.File, receiverType, methodName, generatedCode string) error {
 	var methodFound bool
-	var newDecls []ast.Decl
+	var newDecls []dst.Decl
 
-	// Parse the generated method code to get its AST node
-	genFile, err := parser.ParseFile(token.NewFileSet(), "", "package main \n"+generatedCode, parser.ParseComments)
+	genFile, err := decorator.ParseFile(token.NewFileSet(), "", "package main \n"+generatedCode, parser.ParseComments)
 	if err != nil {
 		return fmt.Errorf("failed to parse generated code: %w", err)
 	}
-	var generatedMethod *ast.FuncDecl
+
+	var generatedMethod *dst.FuncDecl
 	for _, decl := range genFile.Decls {
-		if funcDecl, ok := decl.(*ast.FuncDecl); ok {
+		if funcDecl, ok := decl.(*dst.FuncDecl); ok {
 			if funcDecl.Name.Name == methodName {
 				generatedMethod = funcDecl
 				break
 			}
 		}
 	}
+
 	if generatedMethod == nil {
 		return fmt.Errorf("generated method %s not found in parsed code", methodName)
 	}
 
-	// Iterate over declarations in the existing file
 	for _, decl := range file.Decls {
-		if funcDecl, ok := decl.(*ast.FuncDecl); ok {
-			// Check if this is the method we want to replace
+		if funcDecl, ok := decl.(*dst.FuncDecl); ok {
 			if funcDecl.Name.Name == methodName && funcDecl.Recv != nil {
 				recvType := ""
 				switch recv := funcDecl.Recv.List[0].Type.(type) {
-				case *ast.StarExpr:
-					if ident, ok := recv.X.(*ast.Ident); ok {
+				case *dst.StarExpr:
+					if ident, ok := recv.X.(*dst.Ident); ok {
 						recvType = ident.Name
 					}
-				case *ast.Ident:
+				case *dst.Ident:
 					recvType = recv.Name
 				}
 				if recvType == receiverType {
-					// Replace this method with the generated one
 					methodFound = true
 					newDecls = append(newDecls, generatedMethod)
-					continue // Skip adding the old method
+					continue
 				}
 			}
 		}
 		newDecls = append(newDecls, decl)
 	}
+
 	if !methodFound {
-		// Method not found; append the generated method
 		newDecls = append(newDecls, generatedMethod)
 	}
 
-	// Update the file declarations
+	file.Decls = newDecls
+	return nil
+}
+*/
+
+func insertOrReplaceMethod(file *dst.File, receiverType, methodName, generatedCode string) error {
+	var methodFound bool
+	var newDecls []dst.Decl
+
+	genFile, err := decorator.ParseFile(token.NewFileSet(), "", "package main \n"+generatedCode, parser.ParseComments)
+	if err != nil {
+		return fmt.Errorf("failed to parse generated code: %w", err)
+	}
+
+	var generatedMethod *dst.FuncDecl
+	for _, decl := range genFile.Decls {
+		if funcDecl, ok := decl.(*dst.FuncDecl); ok {
+			if funcDecl.Name.Name == methodName {
+				generatedMethod = funcDecl
+				break
+			}
+		}
+	}
+
+	if generatedMethod == nil {
+		return fmt.Errorf("generated method %s not found in parsed code", methodName)
+	}
+
+	for _, decl := range file.Decls {
+		if funcDecl, ok := decl.(*dst.FuncDecl); ok {
+			if funcDecl.Name.Name == methodName && funcDecl.Recv != nil {
+				recvType := ""
+				switch recv := funcDecl.Recv.List[0].Type.(type) {
+				case *dst.StarExpr:
+					if ident, ok := recv.X.(*dst.Ident); ok {
+						recvType = ident.Name
+					}
+				case *dst.Ident:
+					recvType = recv.Name
+				}
+				if recvType == receiverType {
+					methodFound = true
+					// Preserve existing comments
+					generatedMethod.Decs = funcDecl.Decs
+					newDecls = append(newDecls, generatedMethod)
+					continue
+				}
+			}
+		}
+		newDecls = append(newDecls, decl)
+	}
+
+	if !methodFound {
+		newDecls = append(newDecls, generatedMethod)
+	}
+
 	file.Decls = newDecls
 	return nil
 }
 
 func main() {
-	// Command-line flags
 	var (
 		rootDir      string
 		targetFile   string
@@ -256,76 +301,64 @@ func main() {
 		receiverType = "StoreType"
 	}
 
-	// Step 1: Collect types that implement FileError interface
 	goFiles, err := getGoFiles(rootDir)
 	if err != nil {
 		fmt.Printf("Error reading Go files: %v\n", err)
 		return
 	}
-
 	typeInfos, err := collectTypesAndMethods(goFiles)
 	if err != nil {
 		fmt.Printf("Error collecting types and methods: %v\n", err)
 		return
 	}
 
-	// Identify types implementing FileError interface
 	var fileErrorTypes []*TypeInfo
 	for _, info := range typeInfos {
 		if methodInfo, exists := info.Methods["IsFileError"]; exists {
 			if methodInfo.Params == 0 && methodInfo.Results == 0 {
-				// Method signature matches: no parameters, no results
 				fileErrorTypes = append(fileErrorTypes, info)
 			}
 		}
 	}
 
-	// Step 2: Generate UnmarshalJSON method using text/template
 	methodCode, err := generateUnmarshalJSON(fileErrorTypes, receiverType)
 	if err != nil {
 		fmt.Printf("Error generating UnmarshalJSON method: %v\n", err)
 		return
 	}
 
-	// Step 3: Read and parse the target Go file
 	fset := token.NewFileSet()
 	src, err := ioutil.ReadFile(targetFile)
 	if err != nil {
 		fmt.Printf("Error reading target Go file: %v\n", err)
 		return
 	}
-	fileAST, err := parser.ParseFile(fset, targetFile, src, parser.ParseComments)
+	fileAST, err := decorator.ParseFile(fset, targetFile, src, parser.ParseComments)
 	if err != nil {
 		fmt.Printf("Error parsing target Go file: %v\n", err)
 		return
 	}
 
-	// Step 4: Insert or replace UnmarshalJSON method in the AST
 	err = insertOrReplaceMethod(fileAST, receiverType, "UnmarshalJSON", methodCode)
 	if err != nil {
 		fmt.Printf("Error inserting/replacing method: %v\n", err)
 		return
 	}
 
-	// Step 5: Write the modified AST back to the file
 	var buf bytes.Buffer
-	err = formatNode(&buf, fset, fileAST)
+	err = formatNode(&buf, fileAST)
 	if err != nil {
 		fmt.Printf("Error formatting modified AST: %v\n", err)
 		return
 	}
-
 	err = ioutil.WriteFile(targetFile, buf.Bytes(), 0644)
 	if err != nil {
 		fmt.Printf("Error writing modified Go file: %v\n", err)
 		return
 	}
-
 	fmt.Println("Method successfully inserted/replaced in the target Go file.")
 }
 
-// Helper function to format and print AST node
-func formatNode(w io.Writer, fset *token.FileSet, node interface{}) error {
-	cfg := &printer.Config{Mode: printer.UseSpaces | printer.TabIndent, Tabwidth: 8}
-	return cfg.Fprint(w, fset, node)
+func formatNode(w io.Writer, node *dst.File) error {
+	return decorator.Fprint(w, node)
 }
